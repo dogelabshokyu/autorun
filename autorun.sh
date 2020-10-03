59e919b84020b96d487616ed163680c7d7250268
Hokyu Kim <dogelabshokyu@gmail.com>
2020-10-03 14:59:39 +0900

    commit

--- autorun.sh
+++ autorun.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #For ubuntu 20.04
 #install basic package
-sudo apt-get install -y git curl wget zsh gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0
+sudo apt-get install -y git curl wget zsh gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0 python
+sudo apt install -y bc build-essential ccache curl g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop m4 openjdk-8-jdk pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev bison gperf libxml2-utils make zlib1g-dev:i386 liblz4-tool libncurses5 unzip clang
 #install ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -27,6 +28,6 @@ cd los && repo init -u git://github.com/LineageOS/android.git -b lineage-17.1 &&
 echo Resurrection Remix Q
 cd rr && repo init -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q && cd ..
 echo Pixel Experience ten
-cd pe && repo init -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q && cd ..
+cd pe && repo init -u repo init -u https://github.com/PixelExperience/manifest -b ten && cd ..
 echo sync all
 repo sync
