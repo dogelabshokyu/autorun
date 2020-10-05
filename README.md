18eaf05df6b3bcd7121f56dac8fa9d3f26f76c69
Hokyu Kim <dogelabshokyu@gmail.com>
2020-10-06 00:20:23 +0900

    commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # autorun
 Ez setup
+| Method    | Command                                                                                           |
+|:----------|:--------------------------------------------------------------------------------------------------|
+| **curl**  | `sh -c "$(curl -fsSL https://raw.githubusercontent.com/dogelabshokyu/autorun/master/autorun.sh)"` |
+| **wget**  | `sh -c "$(wget -O- https://raw.githubusercontent.com/dogelabshokyu/autorun/master/autorun.sh)"`   |
+| **fetch** | `sh -c "$(fetch -o - https://raw.githubusercontent.com/dogelabshokyu/autorun/master/autorun.sh)"` |
--- autorun.sh
+++ autorun.sh
@@ -10,10 +10,63 @@
 #########################################
 
 #########################################
+#
 # change mirror server
 # setup build env for android build
+#
 #########################################
 
+#########################################
+#
+#	export shell variables
+#
+#	CMD_V_... : variable
+#	CMD_F_... : file name
+#	CMD_D_... : directory name
+#
+#########################################
+export CMD_V_ANDROID_VERSION=
+export CMD_V_AOSP_YN=
+export CMD_V_LOS_YN=
+export CMD_V_RR_YN=
+export CMD_V_RR_YN=
+export CMD_V_PACKAGE_INSTALL=
+
+export CMD_D_TOP=~/
+export CMD_D_ANDROID_HOME=~/Android
+export CMD_D_AOSP=$CMD_D_ANDROID_HOME/AOSP
+
+#########################################
+#
+#	define functions
+#
+#	FUNC_... : funcion
+#
+#########################################
+
+function FUNC_help()
+{
+	cat <<EOF
+=================================
+=		TEST		=
+=================================
+EOF
+}
+
+function FUNC_PACKAGE()
+{
+	while[ -z $CMD_V_PACKAGE_INSTALL ]
+	do
+		clear
+		echo "********************************************************************************"
+		echo "			Did you wanna install Basic Package? (Y/N)		"
+		echo "********************************************************************************"
+		read $CMD_V_PACKAGE_INSTALL
+	done
+
+	echo "Install Package : $CMD_V_PACKAGE_INSTALL"
+	echo
+	exit
 
 
 #change mirror server to kakao
@@ -21,8 +74,7 @@ sudo sed -i 's/kr.archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
 sudo apt-get update
 
 #Setup build env
-sudo apt-get install -y git curl wget zsh gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0 python bc build-essential ccache curl g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop m4 openjdk-8-jdk pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev bison gperf libxml2-utils make zlib1g-dev:i386 liblz4-tool libncurses5 unzip clang git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
-
+sudo apt-get install -y bc bison bison build-essential build-essential ccache clang curl curl curl flex fontconfig g++-multilib g++-multilib gcc-multilib gcc-multilib gir1.2-clutter-1.0 gir1.2-gtop-2.0 gir1.2-nm-1.0 git git git-core gnupg gnupg gperf gperf imagemagick lib32ncurses5-dev lib32ncurses5-dev lib32readline-dev lib32z1-dev lib32z1-dev libc6-dev-i386 libgl1-mesa-dev liblz4-tool liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libx11-dev libxml2 libxml2-utils libxml2-utils libxml2-utils lzop m4 make openjdk-8-jdk pngcrush python rsync schedtool squashfs-tools unzip unzip wget x11proto-core-dev xsltproc xsltproc zip zip zlib1g-dev zlib1g-dev zlib1g-dev:i386 zsh
 #install Git-repo
 mkdir ~/bin
 PATH=~/bin:$PATH
@@ -56,3 +108,5 @@ cd PE/ten && repo init -u repo init -u https://github.com/PixelExperience/manife
 
 echo sync all
 repo sync
+
+
